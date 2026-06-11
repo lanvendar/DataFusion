e4bcc3a57e4927358550ea6edd30c39d97116fee
lanvendar <liuyanwei75@126.com>
2026-06-11 11:01:13 +0800

    无影响更新

--- datafusion-scheduler-master/src/main/java/com/datafusion/scheduler/master/event/ExactMatchGlobalEventOperator.java
+++ datafusion-scheduler-master/src/main/java/com/datafusion/scheduler/master/event/ExactMatchGlobalEventOperator.java
@@ -1,16 +1,3 @@
-/*
- * Copyright © 2020-2022 Nimbus Corporation All rights reserved.
- *
- * 使本项目源码前请仔细阅读以下协议内容，如果你同意以下协议才能使用本项目所有的功能,
- * 否则如果你违反了以下协议，有可能陷入法律纠纷和赔偿，作者保留追究法律责任的权利.
- *
- * 1、本代码为商业源代码，只允许已授权内部人员查看使用
- * 2、任何人员无权将代码泄露或者授权给其他未被授权人员使用
- * 3、任何修改请保留原始作者信息，不得擅自删除及修改
- *
- * 请保留以上版权信息，否则作者将保留追究法律责任.
- */
-
 package com.datafusion.scheduler.master.event;
 
 import cn.hutool.core.lang.Pair;
